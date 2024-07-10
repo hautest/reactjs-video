2b4cdbb357848547f7684eca0e2746ea88858f96
LeeSeungHun <dos07008@naver.com>
2024-07-11 00:01:18 +0900

    feat: setting typescript, prettier, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react-hooks', 'simple-import-sort'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-console': ['error'],
+  },
+};
